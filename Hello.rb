5c04f31ab351bf9097f0765efaa81492de968642
Akin Onnim <110268633+akinonnim@users.noreply.github.com>
2024-08-27 18:55:59 -0300

    //

---
+++ Hello.rb
@@ -0,0 +1,15 @@
+class Ola_Mundo
+
+  def initialize your_name
+
+    @your_name = your_name
+  end
+
+  def say_hello
+    puts "Bem vindo ao Ruby, #{@your_name}!"
+  end
+end
+
+hello_world = HelloWorld.new "Wesley The Akin"
+
+hello_world.say_hello
